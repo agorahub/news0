08ce0d37f4f8fa5ff38c26b35c797da24be1daa1
theagora <agoradao@gmail.com>
2020-03-02 14:58:19 +0800

    Add gh commit function in _feed.sh

--- _feed.sh
+++ _feed.sh
@@ -5,3 +5,8 @@ ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSS.run({
 ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSS.run({"source"=>"http://rthk9.rthk.hk/rthk/news/rss/c_expressnews_cinternational.xml"})'
 ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSS.run({"source"=>"http://rthk9.rthk.hk/rthk/news/rss/c_expressnews_cfinance.xml"})'
 
+timestamp=`date "+%Y-%m-%d %H:%M"`
+git add _posts/.
+git commit _posts/. -m "Update $timestamp"
+git push
+
