614272c455916ad46702d9e0326faacadf7790a3
theagora <agoradao@gmail.com>
2020-03-06 10:20:54 +0800

    Add OS detection for date fmt in _feed.sh

--- _feed.sh
+++ _feed.sh
@@ -7,9 +7,18 @@ ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSS.run({
 ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSS.run({"source"=>"http://rthk9.rthk.hk/rthk/news/rss/c_expressnews_cinternational.xml"})'
 ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSS.run({"source"=>"http://rthk9.rthk.hk/rthk/news/rss/c_expressnews_cfinance.xml"})'
 
-archstamp=`date --date="7 days ago" +%Y-%m-%d`
+if [ "$(uname -s)" == "Darwin" ]; then
+    archstamp=`date -j -v-7d "+%Y-%m-%d"`
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    archstamp=`date --date="-7 days" +%Y-%m-%d`
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    archstamp=`date --date="-7 days" +%Y-%m-%d`
+fi
+
 timestamp=`date "+%Y-%m-%d %H:%M"`
+
 mv _posts/$archstamp* _posts/_archive/.
+
 git add _posts/.
 git commit _posts/. -m "Update $timestamp"
 git push
