1f5fb96465947e348390693b341180a18f29edbf
theagora <agoradao@gmail.com>
2020-10-01 09:12:28 +0800

    update cmd for rthk posts archive

--- _feed.sh
+++ _feed.sh
@@ -8,16 +8,16 @@ ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSSRTHK.r
 ruby -r rubygems -e 'require "jekyll-import"; JekyllImport::Importers::RSSRTHK.run({"source"=>"http://rthk9.rthk.hk/rthk/news/rss/c_expressnews_cfinance.xml"})'
 
 if [ "$(uname -s)" == "Darwin" ]; then
-    archstamp=`date -j -v-7d "+%Y-%m-%d"`
+    archRTHK=`date -j -v-7d "+%Y-%m-%d"`-RTHK-
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    archstamp=`date --date="-7 days" +%Y-%m-%d`
+    archRTHK=`date --date="-7 days" +%Y-%m-%d`-RTHK-
 elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-    archstamp=`date --date="-7 days" +%Y-%m-%d`
+    archRTHK=`date --date="-7 days" +%Y-%m-%d`-RTHK-
 fi
 
 timestamp=`date "+%Y-%m-%d %H:%M"`
 
-mv _posts/$archstamp* _posts/_archive/.
+mv _posts/$archRTHK* _posts/_archive/.
 
 git add _posts/.
 git commit _posts/. -m "Update $timestamp"
